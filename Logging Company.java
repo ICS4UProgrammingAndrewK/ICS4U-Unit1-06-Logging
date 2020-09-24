41076b4e2a3c76524098c87ccb786e6b8e6f948f
Ubuntu <ubuntu@ip-172-31-19-228.ec2.internal>
2020-09-24 15:58:22 +0000

    Test commit

---
+++ Logging Company.java
@@ -0,0 +1,28 @@
+/**
+*
+* class Logging Company
+*
+* @author  Andrew Kuekam
+* @version 1.0
+* @since   2020-18-09
+*/
+
+import java.util.Scanner;
+
+public class Logging Company 
+{
+   public static void main(String [] args)
+   {
+     double radius;
+    double area;
+    
+    // Know radius
+    Scanner aVar.nextDouble();
+    
+    // apply formula
+    area = radius * radius * 3.14;
+    
+    // output console
+    System.out.println("The Circl's area of radius " + radius + " is " + area);
+   }
+}
