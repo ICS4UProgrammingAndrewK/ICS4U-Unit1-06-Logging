13ffe2a3c4cdae6a19846a9a4852a02120c36303
AndrewK <43419144+AndrewKuekam17@users.noreply.github.com>
2020-10-28 12:32:43 -0400

    Update Logging Company.java

--- Logging Company.java
+++ Logging Company.java
@@ -1,6 +1,6 @@
 /**
 *
-* class Logging Company
+* class LoggingCompany
 *
 * @author  Andrew Kuekam
 * @version 1.0
@@ -9,20 +9,28 @@
 
 import java.util.Scanner;
 
-public class Logging Company 
+public class LoggingCompany 
 {
    public static void main(String [] args)
    {
-     double radius;
-    double area;
-    
-    // Know radius
-    Scanner aVar.nextDouble();
-    
-    // apply formula
-    area = radius * radius * 3.14;
-    
-    // output console
-    System.out.println("The Circl's area of radius " + radius + " is " + area);
+     
+     Scanner userInput = new Scanner(System.in);
+     
+      // declare local varibles
+      double logs;
+      double useLength;
+      
+      // The length of the logs
+      System.out.println (" Enter the lengths of the logs 0.25 m, 0.5 m or 1m ");
+      
+      // Know Length
+      useLength = userInput.nextDouble();
+      
+      // apply formula
+      logs = (1100 / (20 * (useLength)));
+      
+      // output console
+      System.out.println(" The Total Maple Logs in truck is: " + logs
+      );
    }
 }
